82ac1fb582b319809b5eff9d963b51577e389372
LeeSeokBong <kingstar6035@gmail.com>
2022-01-04 21:42:45 +0900

    패캠 강의 Part1_2_05

--- lecture/my-app/src/App.js
+++ lecture/my-app/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 
 import Extraction from './Part1_2_04/Extraction';
-import Composition from './Part1_2_04/Part1_2_04';
+import Composition from './Part1_2_04/Composition';
+import ClassComponent from './Part1_2_05/ClassComponent';
+import FunctionalComponent from './Part1_2_05/FunctionalComponent';
 
 function App() {
   return (
     <div className="App">
+      <FunctionalComponent/>
+      <ClassComponent/>
       <Extraction/>
       <Composition/>
     </div>
